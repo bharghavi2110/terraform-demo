d31ced6f6a9c385841dc7288424012991842c270
Rathina Bharghavi <rathinabharghavi@gmail.com>
2025-05-06 21:17:22 +0530

    First Terraform commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyFirstEC2"
+  }
+}
